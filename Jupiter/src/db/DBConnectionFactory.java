b5485f1d444e78790f23c04a2b9bec295c6798de
zhaolinzhang <zz1296@nyu.edu>
2018-10-08 14:31:52 -0400

    mongodb

--- Jupiter/src/db/DBConnectionFactory.java
+++ Jupiter/src/db/DBConnectionFactory.java
@@ -1,5 +1,6 @@
 package db;
 
+import db.mongodb.MongoDBConnection;
 import db.mysql.MySQLConnection;
 
 public class DBConnectionFactory {
@@ -11,8 +12,7 @@ public class DBConnectionFactory {
 		case "mysql":
 			return new MySQLConnection();
 		case "mongodb":
-			// return new MongoDBConnection();
-			return null;
+			return new MongoDBConnection();
 		default:
 			throw new IllegalArgumentException("Invalid db: " + db);
 		}
